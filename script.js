8ecf6ca5baa54c7d1d1dd1bd1968cb4b45ddc62b
Bram Brink <bram@brink.email>
2022-02-08 12:03:50 +0100

    Implemented responsive header bar, first version

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const navButton = document.querySelector('.header__nav-button');
-const navButtonImage = document.querySelector('.header__nav-button__image');
+const navButton = document.querySelector('.header__nav-button--mobile');
+const navButtonImage = document.querySelector('.header__nav-button__image--mobile');
 let menuOpen = false;
 
 const article = document.querySelector('article');
-const navMenu = document.querySelector('nav');
+const navMenu = document.querySelector('.navigation--mobile');
 
 const toggleMenu = () => {
     if(!menuOpen) {
-        navButtonImage.src = 'images/Close.svg'
+        navButtonImage.src = 'images/close.svg'
         article.style.setProperty('display','none');
         navMenu.style.setProperty('display','flex');
         menuOpen = true;
     } else {
-        navButtonImage.src = 'images/Hamburger.svg'
+        navButtonImage.src = 'images/hamburger.svg'
         article.style.setProperty('display','flex');
         navMenu.style.setProperty('display','none');
         menuOpen = false;  
