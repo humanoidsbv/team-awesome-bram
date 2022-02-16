e3403cee6cb4eb4c7b49a16a482956667ed80dfb
Bram Brink <bram@brink.email>
2022-02-16 17:03:59 +0100

    Implement other way of grouping days

--- src/components/time-entries/TimeEntries.tsx
+++ src/components/time-entries/TimeEntries.tsx
@@ -30,39 +30,65 @@ export const TimeEntries = () => {
     year: "2-digit",
   };
 
-  const timeEntriesDates = Array.from(
-    timeEntries.reduce(
-      (dates, timeEntry) =>
-        dates.add(new Date(timeEntry.startTimestamp).toLocaleDateString("en-EN", dateOptions)),
-      new Set(),
-    ),
-  ) as string[];
+  // const timeEntriesDates = Array.from(
+  //   timeEntries.reduce(
+  //     (dates, timeEntry) =>
+  //       dates.add(new Date(timeEntry.startTimestamp).toLocaleDateString("en-EN", dateOptions)),
+  //     new Set(),
+  //   ),
+  // ) as string[];
 
-  const timeEntriesDatesArray = [...timeEntriesDates].map((timeEntryDate) =>
-    timeEntries.filter(
-      (timeEntry) =>
-        new Date(timeEntry.startTimestamp).toLocaleDateString("en-EN", dateOptions) ==
-        timeEntryDate,
-    ),
-  );
+  // const timeEntriesDatesArray = [...timeEntriesDates].map((timeEntryDate) =>
+  //   timeEntries.filter(
+  //     (timeEntry) =>
+  //       new Date(timeEntry.startTimestamp).toLocaleDateString("en-EN", dateOptions) ==
+  //       timeEntryDate,
+  //   ),
+  // );
+
+  const dateOptionsSort: {} = {
+    day: "numeric",
+    month: "numeric",
+    year: "2-digit",
+  };
+
+  timeEntries.sort((a, b) => (new Date(a.startTimestamp) < new Date(b.startTimestamp) ? -1 : 1));
+
+  console.log(timeEntries);
+
+  const renderHeader = () => {};
 
   return (
     <Styled.TimeEntries>
-      {timeEntriesDatesArray.map((timeEntries, index) => {
+      {timeEntries.map((timeEntry, index, array) => {
         return (
-          <div key={timeEntriesDates[index]}>
-            <Styled.TimeEntryHeader>
-              <p>{timeEntriesDates[index]}</p>
-              <p>08:00</p>
-            </Styled.TimeEntryHeader>
+          <div key={timeEntry.id}>
+            {index >= 1 ? (
+              new Date(timeEntry.startTimestamp).toLocaleDateString("en-EN", dateOptionsSort) >
+                new Date(timeEntries[index - 1].startTimestamp).toLocaleDateString(
+                  "en-EN",
+                  dateOptionsSort,
+                ) && (
+                <Styled.TimeEntryHeader>
+                  <p>
+                    {new Date(timeEntry.startTimestamp).toLocaleDateString("en-EN", dateOptions)}
+                  </p>
+                  <p>08:00</p>
+                </Styled.TimeEntryHeader>
+              )
+            ) : (
+              <Styled.TimeEntryHeader>
+                <p>{new Date(timeEntry.startTimestamp).toLocaleDateString("en-EN", dateOptions)}</p>
+                <p>08:00</p>
+              </Styled.TimeEntryHeader>
+            )}
             <Styled.TimeEntryContainer>
-              {timeEntries.map((timeEntry) => (
-                <TimeEntry {...timeEntry} key={timeEntry.id} />
-              ))}
+              <TimeEntry {...timeEntry} key={timeEntry.id} />
             </Styled.TimeEntryContainer>
           </div>
         );
       })}
+
       <Button label="Add time entry" onClick={handleClick} />
     </Styled.TimeEntries>
   );
