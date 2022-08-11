04bc95a59ea66ba73037415615ca0441ae445f9b
Bram <bram.brink@humanoids.nl>
2022-08-11 22:19:20 +0200

    commiting changes

--- apollo-client.js
+++ apollo-client.js
@@ -4,17 +4,6 @@ export const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
     uri: "http://localhost:3333",
-    credentials: "same-origin",
   }),
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: "no-cache",
-      errorPolicy: "ignore",
-    },
-    query: {
-      fetchPolicy: "no-cache",
-      errorPolicy: "all",
-    },
-  },
 });
